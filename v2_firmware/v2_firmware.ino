5ac3e490a50d78eaf6dcf8b54ac8e1ac89ac3c16
Nick Witten <nwitt12@gmail.com>
2022-11-28 14:27:08 -0500

    v2_firmware waveforms and driver functions

--- v2_firmware/v2_firmware.ino
+++ v2_firmware/v2_firmware.ino
@@ -1,63 +1,121 @@
 #define SYS_FREQ 16000000
+#define TCA_FREQ 4000000
 #define BOOST_FREQ 10000
-#define PULSE_FREQ 500
-#define PULSE_DUTY 0.2
 
-uint32_t boost_period_ticks;
-uint32_t pulse_period_ovf = BOOST_FREQ / PULSE_FREQ;  // Amount of TCA0 overflows for pulse period
-uint32_t pulse_duty_ovf = BOOST_FREQ / PULSE_FREQ * PULSE_DUTY;  // Amount of TCA0 overflows for pulse duty
+
+const uint32_t boost_period_ticks = TCA_FREQ / BOOST_FREQ;
+const uint8_t pulse_delay_ticks = 1;
+
 volatile uint32_t ovf_count = 0;
-volatile float boost_duty = 0.3;
-uint32_t TCA_frequency;
+volatile uint8_t dir = 0;
+
+volatile uint32_t pulse_period_ovf;
+volatile uint32_t pulse_duty_ovf;
+volatile float boost_duty;
+volatile uint8_t waveform_type;
+
+enum waveform {
+    OFF,
+    PULSE,
+    DIFFERENTIAL,
+    ASCENDING,
+    ASCENDING_DIFFERENTIAL
+};
+
+void waveform_param_set(uint32_t frequency, float duty, float amplitude, enum waveform type) {
+    if (type == OFF) {
+        TCA0.SINGLE.CTRLA &= ~TCA_SINGLE_ENABLE_bm;
+    } else {
+        TCA0.SINGLE.CTRLA |= TCA_SINGLE_ENABLE_bm;
+    }
+    pulse_period_ovf = BOOST_FREQ / frequency;  // Amount of TCA0 overflows for pulse period
+    pulse_duty_ovf = BOOST_FREQ / frequency * duty;  // Amount of TCA0 overflows for pulse duty
+    boost_duty = amplitude;
+    waveform_type = type;
+}
+
+/* Turn on the high side to either output line
+   direction:
+        0 - line 1
+        >= 1 - line 2
+*/
+static inline void set_pulse_line_on(uint8_t direction) {
+    if (direction) {
+        PORTF.OUTCLR = PIN5_bm;  // Turn off H_DRIVE1
+        PORTC.OUTCLR = PIN6_bm;  // Turn off L_DRIVE2
+        PORTA.OUTSET = PIN0_bm;  // Turn on L_DRIVE1
+        PORTF.OUTSET = PIN4_bm;  // Turn on H_DRIVE2
+    } else {
+        PORTF.OUTCLR = PIN4_bm;  // Turn off H_DRIVE2
+        PORTA.OUTCLR = PIN0_bm;  // Turn off L_DRIVE1
+        PORTC.OUTSET = PIN6_bm;  // Turn on L_DRIVE2
+        PORTF.OUTSET = PIN5_bm;  // Turn on H_DRIVE1
+    }
+}
+
+static inline void set_pulse_line_off(void) {
+    PORTF.OUTCLR = PIN5_bm;  // Turn off H_DRIVE1
+    PORTF.OUTCLR = PIN4_bm;  // Turn off H_DRIVE2
+}
+
+void pulse_init(void) {
+    // Digital writes
+    PORTF.DIRSET = PIN5_bm;  // H_DRIVE1
+    PORTF.DIRSET = PIN4_bm;  // H_DRIVE2
+    PORTA.DIRSET = PIN0_bm;  // L_DRIVE1
+    PORTC.DIRSET = PIN6_bm;  // L_DRIVE2
+
+    // PWM TCA Output Channel
+    PORTB.DIRSET = PIN2_bm;  // D5, PB2 TCA_CMP2
+
+    // Timer A Setup
+    #ifdef TCA_FREQ
+    TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV4_gc;  // Set clock
+    #endif
+    TCA0.SINGLE.CTRLB = TCA_SINGLE_CMP2EN_bm | TCA_SINGLE_WGMODE_SINGLESLOPE_gc;  // Single slope and enable channel 2
+    TCA0.SINGLE.PERBUF = boost_period_ticks;  // Set the period
+    TCA0.SINGLE.CMP2BUF = 0;  // Set the duty-cycle
+    TCA0.SINGLE.INTCTRL = TCA_SINGLE_CMP2_bm | TCA_SINGLE_OVF_bm;  // Setup interrupts to fire on cmp2  
+
+    // Initialize timing parameters
+    waveform_param_set(100, 0, 0, OFF);
+
+    // Enable timer
+    TCA0.SINGLE.CTRLA |= TCA_SINGLE_ENABLE_bm;
+}
+
+
 void setup() {
-  // Digital writes
-  PORTF.DIRSET = PIN5_bm;  // H_DRIVE1
-  PORTF.DIRSET = PIN4_bm;  // H_DRIVE2
-  PORTA.DIRSET = PIN0_bm;  // L_DRIVE1
-  PORTC.DIRSET = PIN6_bm;  // L_DRIVE2
-
-  // PWM TCA Output Channel
-  PORTB.DIRSET = PIN2_bm;  // D5, PB2 TCA_CMP2
-
-  // Timer A Setup
-  TCA_frequency = SYS_FREQ / 1;
-  boost_period_ticks = TCA_frequency / BOOST_FREQ;
-
-  TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV1_gc;  // Set clock
-  TCA0.SINGLE.CTRLB = TCA_SINGLE_CMP2EN_bm | TCA_SINGLE_WGMODE_SINGLESLOPE_gc;  // Single slope and enable channel 2
-  TCA0.SINGLE.PERBUF = boost_period_ticks;  // Set the period
-  TCA0.SINGLE.CMP2BUF = 0;  // Set the duty-cycle
-  TCA0.SINGLE.INTCTRL = TCA_SINGLE_CMP2_bm | TCA_SINGLE_OVF_bm;  // Setup interrupts to fire on cmp2  
-
-  TCA0.SINGLE.CTRLA |= TCA_SINGLE_ENABLE_bm;
-
-//  Serial.begin(9600);
-//  Serial.write("hi");
+    pulse_init();
+    // Initialize timing parameters
+    waveform_param_set(200, 0.1, 0.5, DIFFERENTIAL);
 }
 
 void loop() {}
 
 ISR(TCA0_CMP2_vect) {
-  TCA0.SINGLE.INTFLAGS |= TCA_SINGLE_CMP2_bm;
+    TCA0.SINGLE.INTFLAGS |= TCA_SINGLE_CMP2_bm;
 }
 
-uint8_t pulse_delay = 1;
 ISR(TCA0_OVF_vect) {
-  if (ovf_count <= pulse_duty_ovf + pulse_delay) {
-    TCA0.SINGLE.CMP2BUF = boost_period_ticks * boost_duty;  // Set the duty-cycle
-    if (ovf_count >= pulse_delay) {
-        PORTF.OUTSET = PIN5_bm;  // Turn on H_DRIVE1
+    if (ovf_count < pulse_duty_ovf + pulse_delay_ticks) {
+        TCA0.SINGLE.CMP2BUF = boost_period_ticks * boost_duty;  // Set the duty-cycle
+        if (ovf_count >= pulse_delay_ticks) {
+            set_pulse_line_on(dir);
+        }
+    } else {
+        set_pulse_line_off();
+        TCA0.SINGLE.CMP2BUF = 0;
     }
-  } else {
-    PORTF.OUTCLR = PIN5_bm;
-    TCA0.SINGLE.CMP2BUF = 0;
-  }
 
-  if (ovf_count >= pulse_period_ovf - 1) {
-    ovf_count = 0;
-  } else {
-    ovf_count++;
-  }
+    if (ovf_count >= pulse_period_ovf - 1) {  // End of pulse period
+        if (waveform_type == DIFFERENTIAL | waveform_type == ASCENDING_DIFFERENTIAL) {
+            dir = ~dir;
+        }
+        ovf_count = 0;
+    } else {
+        ovf_count++;
+    }
 
-  TCA0.SINGLE.INTFLAGS |= TCA_SINGLE_OVF_bm;  // Clear interrupt flag
+    TCA0.SINGLE.INTFLAGS |= TCA_SINGLE_OVF_bm;  // Clear interrupt flag
 }
