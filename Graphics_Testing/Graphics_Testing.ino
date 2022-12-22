58a52b52f3fa0732dd1f4db0df1e97b01d85eb70
Nick Witten <nwitt12@gmail.com>
2022-12-22 17:11:56 -0500

    last bit of files

--- Graphics_Testing/Graphics_Testing.ino
+++ Graphics_Testing/Graphics_Testing.ino
@@ -1,4 +1,5 @@
 #include <Arduino_GFX_Library.h>
+#include <arduino-timer.h>
 
 
 #define GFX_BL 9 // default backlight pin, you may replace df_gfx_bl to actual backlight pin
@@ -12,81 +13,123 @@ Arduino_DataBus *bus = new Arduino_HWSPI(GFX_DC, GFX_NOT_DEFINED);
 Arduino_GFX *gfx = new Arduino_GC9A01(bus, GFX_RST, 0 /* rotation */, false /* ips */);
 
 #define CURVE_WIDTH 8
-uint8_t curve_end[CURVE_WIDTH] = {
-//     0b10000000,
-//     0b11111000,
-//     0b11111110,
-//     0b11111111,
-//     0b11111111,
-//     0b11111110,
-//     0b11111000,
-//     0b10000000,
-    0b00011000,
-    0b00111100,
-    0b00111100,
-    0b01111110,
-    0b01111110,
-    0b01111110,
-    0b01111110,
-    0b11111111,
-};
-
-
-float curve_position;
-
-#define CURVE_RES 10
-#define CURVE_RAD 100
 #define CURVE_COLOR 0XEEA8
+#define BG_COLOR 0xFFFF
 #define MAX_X 240
 #define MAX_Y 240
-// Position is a 0 - 32 int
-void update_curve(float update_position) {
-    if (update_position > curve_position) {
-        do {
-            gfx->fillArc(MAX_X / 2, MAX_Y / 2, CURVE_RAD, CURVE_RAD - CURVE_WIDTH, curve_position, curve_position + CURVE_RES, CURVE_COLOR);
-            // gfx->fillArc(MAX_X / 2, MAX_Y / 2, CURVE_RAD, CURVE_RAD - CURVE_WIDTH, 0.0, 180.0, CURVE_COLOR);
-            curve_position += CURVE_RES;
-        } while (curve_position < update_position);
+
+uint8_t enc1 = 16;
+uint8_t enc2 = 17;
+
+int curve_position = 180;
+int val = 50;
+
+Timer<10, micros> timer;
+
+void update_curve(int update_position, int curve_rad) {
+    // update_position = 10 * (update_position / 10);  // Round down to closest 10
+    if (update_position == curve_position) {
+        return;
     }
-//     while (curve_position >= update_position) {
-//         for (int i = 0; i < CURVE_WIDTH * 8; i++) {
-//             uint8_t x = i % 8;
-//             uint8_t y_inv = i / 8;
-//             uint16_t color = (curve_end[y_inv] & 1 << x) ? 0xFFFF : 0x4444;
-//             float mag = sqrt(pow(x, 2) + pow(y_inv, 2));
-//             float ang = atan((float)y_inv / x);
-//             ang += curve_position * CURVE_RES;
-//             x = mag * cos(ang);
-//             y_inv = mag * sin(ang);
-//             x += CURVE_RAD * cos(curve_position * CURVE_RES) + MAX_X / 2;
-//             y_inv += CURVE_RAD * sin(curve_position * CURVE_RES) + MAX_Y / 2;
-//             gfx->startWrite();
-//             gfx->writePixelPreclipped(x, MAX_Y - y_inv, color);
-//         }
-//         curve_position--;
-//     }
+    if (update_position > curve_position) {
+        gfx->fillArc(MAX_X / 2, MAX_Y / 2, curve_rad, curve_rad - CURVE_WIDTH, curve_position, update_position, CURVE_COLOR);
+    } else {
+        gfx->fillArc(MAX_X / 2, MAX_Y / 2, curve_rad, curve_rad - CURVE_WIDTH, update_position, curve_position, BG_COLOR);
+    }
+    curve_position = update_position;
+}
+
+void erase_number(void) {
+    int start_x = MAX_X / 2 - 50 * (3.0 / 2);
+    int start_y = MAX_Y / 2 - 30;
+    int end_x = MAX_X / 2 + 50 * (3.0 / 2);
+    int end_y = MAX_Y / 2 + 30;
+    gfx->fillRect(start_x, start_y, end_x - start_x, end_y - start_y, BG_COLOR);
+}
+
+
+void update_number(const char *c, int size) {
+    static int prev_size;
+    if (prev_size != size) {
+        erase_number();
+    }
+    int i = 0;
+    while (*c != NULL) {
+        Serial.print(c);
+        int x = MAX_X / 2 + 20 * (i - (float)size / 2);
+        int y = MAX_Y / 2;
+        Serial.print(x);
+        gfx->drawChar(x, y, *c, CURVE_COLOR, BG_COLOR);
+        c++;
+        i++;
+    }
+    prev_size = size;
+}
+
+void update_value(int *value, int new_value, int minimum, int maximum) {
+    if (new_value > maximum) {
+        *value = maximum;
+    } else if (new_value < minimum) {
+        *value = minimum;
+    } else {
+        *value = new_value;
+    }
+    update_curve(180 + 180.0f / (maximum - minimum) * (*value), 100);
+    char num_string_buf[4];
+    int size = snprintf(num_string_buf, 4, "%d", *value);
+    update_number(num_string_buf, size);
 }
 
 void setup(void)
 {
-    curve_position = 180.0;
-
     gfx->begin();
-    gfx->fillScreen(0xFFFF);
+    gfx->fillScreen(BG_COLOR);
+    gfx->setTextSize(3);
 
     pinMode(GFX_BL, OUTPUT);
     digitalWrite(GFX_BL, HIGH);
 
+    pinMode(enc1, INPUT_PULLUP);
+    pinMode(enc2, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(enc1), encoder_ISR, CHANGE);
+
+    Serial.begin(9600);
 }
 
 void loop()
 {
-    update_curve(270.0);
-    delay(2000);
-    update_curve(360);
-    update_curve(270.0);
-    delay(2000);
-    update_curve(0);
-    delay(2000);
+//     while (val < 100) {
+//         update_value(&val, val + 10, 0, 100);
+//         delay(1000);
+//     }
+//     while (val > 0) {
+//         update_value(&val, val - 10, 0, 100);
+//         delay(1000);
+//     }
+    timer.tick();
 }
 
+bool encoder_stable;
+void encoder_update(void) {
+    timer.cancel();
+    Serial.print(encoder_stable);
+    if (!encoder_stable) {
+        encoder_stable = true;
+        timer.in(500, encoder_update);
+        return;
+    }
+    int8_t dir;
+    if (digitalRead(enc1) == digitalRead(enc2)) {
+        dir = -1;
+    } else {
+        dir = 1;
+    }
+    update_value(&val, val + dir, 0, 100);
+}
+
+
+
+void encoder_ISR(void) {
+    encoder_stable = false;
+    encoder_update();
+}
